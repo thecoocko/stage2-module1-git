11dc49b9ff8276e0328ae2336dba889099d52529
thecoocko <butenkonaddezhda@gmail.com>
2023-08-23 12:59:12 +0200

    done: part1

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/thecoocko/stage2-module1-git-empty.git";
+    public static String REPO_LINK = "https://github.com/thecoocko/glowing-fiesta.git";
 }
